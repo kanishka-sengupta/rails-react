271daf2577224faacb4942bba2381066a5211c4b
kanishka-sengupta <kanishka.sengupta@zemosolabs.com>
2021-06-16 01:14:35 +0530

    Recipe creation

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,13 +1,37 @@
 class Api::V1::RecipesController < ApplicationController
   def index
+    recipes=Recipe.all.order(created_at: :desc)
+    render json: recipes
   end
 
   def create
+    recipe=Recipe.create!(recipe_params)
+    if recipe
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
   end
 
   def show
+    if recipe
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
   end
 
   def destroy
+    recipe&.destroy
+    render json: {message: 'Recipe Deleted'}
+  end
+
+  private
+  def recipe_params
+    params.permit(:name,:ingredients,:instruction)
+  end
+
+  def recipe
+    @recipe ||=Recipe.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       get 'recipes/index'
-      get 'recipes/create'
-      get 'recipes/show'
-      get 'recipes/destroy'
+      post 'recipes/create'
+      get '/show/:id', to: 'recipes#show'
+      get '/destroy/:id', to: 'recipes#destroy'
     end
   end
   root 'home_page#index'
+  get '/*path' => 'home_page#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
